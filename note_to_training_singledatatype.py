c2dd3b45434578bdf2439dbd906171c05704e5f1
Zhi WEN <zwen8@ds2.CS.McGill.CA>
2019-11-25 14:37:27 -0500

    single note type version

--- note_to_training_singledatatype.py
+++ note_to_training_singledatatype.py
@@ -13,30 +13,18 @@ from multiprocessing import Pool
 from itertools import repeat
 import time
 
-# categories and type ids
-category_to_id = {
-    'Physician ': '1',
-    'Nursing': '2',
-    'Respiratory ': '3',
-    'Nursing/other': '4',
-}
-
 def parse_args():
-    parser = argparse.ArgumentParser(description='Preprocess notes, extract ventilation duration, and prepare for running MixEHR. Differentiating between different data types.')
+    parser = argparse.ArgumentParser(description='Preprocess notes, extract ventilation duration, and prepare for running MixEHR')
     parser.add_argument('input_file_path', help='path to the original file')
     parser.add_argument('output_directory', help='directory to store outputs')
     parser.add_argument('--no_data', action='store_true', help='NOT outputing data')
-    parser.add_argument('--use_vocab', help='specify directory containing the vocabulary to use. setting no_vocab to true')
+    parser.add_argument('--use_vocab', help='specify vocabulary file to use. setting no_vocab to true')
     parser.add_argument('--no_vocab', action='store_true', help='NOT storing vocabulary')
     parser.add_argument('-m', '--meta_data', action='store_true', help='output meta data (default not)')
     parser.add_argument('-v', '--ventilation_duration', action='store_true', help='output ventilation duration (default not)')
     parser.add_argument('--binarized_mv', action='store_true', help='using Label in files that do not have continuous MV durations')
     parser.add_argument('-s', '--split_datasets', action='store_true', help='split the whole cohort into train/validation/test (default not)')
     parser.add_argument('--discard_ids', help='csv file containing the HADM_IDs that should be discarded')
-    parser.add_argument('--no_phy', action='store_true', help='NOT using physicians notes')
-    parser.add_argument('--no_nur', action='store_true', help='NOT using nurses notes')
-    parser.add_argument('--res', action='store_true', help='using respiratory notes')
-    parser.add_argument('--other', action='store_true', help='using nursing/other notes. WARNING: this category contain other types of notes e.g. discharge summary, use with caution')
     parser.add_argument('--max_procs', type=int, help='maximal number of processes (default 10)', default=10)
     return parser.parse_args()
 
@@ -62,18 +50,9 @@ def print_args(args):
         print('Splitting into train/valiation/test')
     if args.discard_ids:
         print('Discarding HADM_IDs in', args.discard_ids)
-    if args.no_phy:
-        print('Not using physician notes')
-    if args.no_nur:
-        print('Not using nursing notes')
-    if args.res:
-        print('Using respiratory notes')
-    if args.other:
-        print('Using nursing/other notes')
 
-def merge_notes(hadm_id, category):
-    tmp_data = data[data.HADM_ID == hadm_id]
-    return "\n".join(tmp_data[tmp_data.CATEGORY == category].TEXT)
+def merge_notes(hadm_id):
+    return "\n".join(data[data.HADM_ID == hadm_id].TEXT)
 
 def preprocess_text(text):
     # convert to lower case
@@ -92,8 +71,8 @@ def preprocess_text(text):
     return no_sw
 
 def get_unigram_counts(text):
-#     text_word = [sentence.split() for sentence in text]
-    text_word = [text.split()]
+    text_word = [sentence.split() for sentence in text]
+#     text_word = [text.split()]
     text_unigram = [ngrams(sentence, 1) for sentence in text_word]
     return NgramCounter(text_unigram)
 
@@ -102,26 +81,25 @@ def get_word_index(data, max_df=0.15, min_df=5):
     vectorizer.fit(data['PROCTEXT'])
     return {word: idx for idx, word in enumerate(vectorizer.get_feature_names())}
 
-def create_output_dataframe(data, word_indexes, args):
+def create_output_dataframe(data, word_index, args):
     ids = data["HADM_ID"].unique().tolist()  # HADM_IDs
     output = []
     ventilation_duration = []
     for idx, id in tqdm(enumerate(ids)):
         set_ventilation = False # indicate whether ventilation duration is calculated for current HADM_ID
         if not args.no_data or args.ventilation_duration:
-            for _, row in data[data['HADM_ID'] == id].iterrows(): # for one HADM_ID - CATEGORY pair
-                unigram_counts = get_unigram_counts(row["PROCTEXT"])
-                for word in set(row["PROCTEXT"].split()):
-                    if word not in word_indexes[row['CATEGORY']].keys():
-                        continue
-                    if not args.no_data:
-                        output.append(" ".join([str(idx), category_to_id[row['CATEGORY']], str(word_indexes[row['CATEGORY']][word]), "0", str(unigram_counts[word])]))
-                    if args.ventilation_duration and not set_ventilation:
-                        set_ventilation = True
-                        if not args.binarized_mv:
-                            ventilation_duration.append(" ".join([str(idx), str(data[data.HADM_ID == id]["DURATION"].values[0])]))
-                        else:
-                            ventilation_duration.append(" ".join([str(idx), str(data[data.HADM_ID == id]["BI_DURATION"].values[0])]))
+            unigram_counts = get_unigram_counts(data[data.HADM_ID == id]["PROCTEXT"].tolist())
+            for word in set(data[data.HADM_ID == id]["PROCTEXT"].values[0].split()):
+                if word not in word_index.keys():
+                    continue
+                if not args.no_data:
+                    output.append(" ".join([str(idx), "1", str(word_index[word]), "0", str(unigram_counts[word])]))
+                if args.ventilation_duration and not set_ventilation:
+                    set_ventilation = True
+                    if not args.binarized_mv:
+                        ventilation_duration.append(" ".join([str(idx), str(data[data.HADM_ID == id]["DURATION"].values[0])]))
+                    else:
+                        ventilation_duration.append(" ".join([str(idx), str(data[data.HADM_ID == id]["BI_DURATION"].values[0])]))
     
 #     for idx, id in tqdm(enumerate(ids)):
 #         set_ventilation = False # indicate whether ventilation duration is calculated
@@ -141,8 +119,6 @@ if __name__ == '__main__':
     args = parse_args()
     if args.no_vocab and args.no_data and not args.meta_data and not args.ventilation_duration:
         raise Exception('no output specified')
-    if args.no_phy and args.no_nur and not args.res:
-        raise Exception('not using any type of note (physicians, nurses, respiratory, nursing/other)')
     if args.use_vocab:
         args.no_vocab = True
         
@@ -151,20 +127,9 @@ if __name__ == '__main__':
 
     data = pd.read_csv(args.input_file_path)
     print('[' + time.ctime() + ']', "data read")
-
-    categories = []
-    if not args.no_phy:
-        categories.append('Physician ')
-    if not args.no_nur:
-        categories.append('Nursing')
-    if args.res:
-        categories.append('Respiratory ')
-    if args.other:
-        categories.append('Nursing/other')
-    args.categories = categories
     
     # merge notes of same HADM_ID of specified categories
-    merged_data = data[data['CATEGORY'].isin(categories)][["HADM_ID", "CATEGORY"]].drop_duplicates()
+    merged_data = data[["HADM_ID"]].drop_duplicates()
     if args.discard_ids:
         discard_ids_df = pd.read_csv(args.discard_ids, header=None)
         discard_ids_df.columns = ['HADM_ID']
@@ -172,9 +137,9 @@ if __name__ == '__main__':
         merged_data = merged_data[~merged_data['HADM_ID'].isin(discard_ids)]
         print('[' + time.ctime() + ']', 'discarding', len(discard_ids), 'HADM_IDs')
     with Pool(processes=args.max_procs) as pool:
-        merged_data["ALLTEXT"] = pool.starmap(merge_notes, zip(merged_data['HADM_ID'], merged_data['CATEGORY']))
+        merged_data["ALLTEXT"] = pool.map(merge_notes, merged_data['HADM_ID'])
     merged_data.dropna()
-    print('[' + time.ctime() + ']', 'after merging notes of same HADM_ID and same CATEGORY, we have', merged_data.shape[0], 'unique HADM_ID - CATEGORY pairs')
+    print('[' + time.ctime() + ']', 'after merging notes of same HADM_ID, we have', merged_data.shape[0], 'unique HADM_IDs')
 
     # preprocess notes
     with Pool(processes=args.max_procs) as pool:
@@ -214,41 +179,36 @@ if __name__ == '__main__':
     # generate word indexes
     if not args.use_vocab:
         if args.split_datasets:
-            word_indexes = {category: get_word_index(train_data[train_data['CATEGORY'] == category]) for category in categories}
+            word_index = get_word_index(train_data)
         else:
-            word_indexes = {category: get_word_index(merged_data[merged_data['CATEGORY'] == category]) for category in categories}
-        print('[' + time.ctime() + ']', 'word indexes generated')
+            word_index = get_word_index(merged_data)
+        print('[' + time.ctime() + ']', 'word index generated')
     else:
-        word_indexes = {}
-        for category in categories:
-            with open(os.path.join(args.use_vocab, category_to_id[category] + '_vocab.txt'), 'r') as file:
-                vocab = [line.rstrip('\n') for line in file.readlines()]
-            word_indexes[category] = {line.split(',')[0]: line.split(',')[1] for line in vocab}
-        print('[' + time.ctime() + ']', 'read word indexes from', args.use_vocab)
+        with open(args.use_vocab, 'r') as file:
+            vocab = [line.rstrip('\n') for line in file.readlines()]
+        word_index = {line.split(',')[0]: line.split(',')[1] for line in vocab}
+        print('[' + time.ctime() + ']', 'read word index from', args.use_vocab)
 
     # store vocabulary
     if not args.no_vocab:
-        for category in categories:
-            word_index_output = [','.join([word, str(idx)]) for word, idx in word_indexes[category].items()]
-            with open(os.path.join(args.output_directory, category_to_id[category] + '_vocab.txt'), "w") as file:
-                file.writelines('\n'.join(word_index_output))
+        word_index_output = [','.join([word, str(idx)]) for word, idx in word_index.items()]
+        with open(os.path.join(args.output_directory, 'vocab.txt'), "w") as file:
+            file.writelines('\n'.join(word_index_output))
         print('[' + time.ctime() + ']', 'vocabulary written to', args.output_directory)
             
     # create output dataframe
     if not args.split_datasets:
-        output, ventilation_duration = create_output_dataframe(merged_data, word_indexes, args)
+        output, ventilation_duration = create_output_dataframe(merged_data, word_index, args)
     else:
         with Pool(processes=args.max_procs) as pool:
             (train_output, train_ventilation_duration), (valid_output, valid_ventilation_duration), (test_output, test_ventilation_duration) = \
-            pool.starmap(create_output_dataframe, zip([train_data, valid_data, test_data], repeat(word_indexes), repeat(args)))
+            pool.starmap(create_output_dataframe, zip([train_data, valid_data, test_data], repeat(word_index), repeat(args)))
         
     print('[' + time.ctime() + ']', "data ready")
     
     # create meta data
     if args.meta_data:
-        meta_data = []
-        for category in categories:
-            meta_data += [" ".join([category_to_id[category], str(idx), "1"]) for idx in word_indexes[category].values()]
+        meta_data = [" ".join(["1", str(idx), "1"]) for idx in word_index.values()]
         print('[' + time.ctime() + ']', "meta data ready")
     
     # write to output
